b753f09c5f1dc7ae4aa578d9c080017271a7f9fb
David Cana <david.javapagetemplates@gmail.com>
2024-12-04 19:20:01 +0100

    Routes added to options in sample. sample.gcc.js replaced by sample.concat.js in sample.html.

--- samples/sample.js
+++ samples/sample.js
@@ -1,8 +1,55 @@
-"use strict";
+// Sample
 
+// Initialize options
 let options = {
     browserHistoryOnLoad: true
 };
 
+// Add routes to options
+options.routes = [
+    // Home page
+    {
+        'path': '',
+        'content': `
+<div class="page" id="homePage">
+    <h3>Home page</h3>
+    This is the Home page
+</div>
+`
+    },
+    // About page
+    {
+        'path': 'about',
+        'content': `
+<div class="page" id="aboutPage">
+    <h3>About page</h3>
+    This is the about page
+</div>
+`
+    },
+    // Links page
+    {
+        'path': 'links',
+        'content': `
+<div class="page" id="linksPage">
+    <h3>Links page</h3>
+    This is the links page
+</div>
+`
+    },
+    // Default route (404 page)
+    {
+        'path': '(404)',
+        'content': `
+<div class="page" id="404Page">
+    <h3>404 page</h3>
+    <p>Sorry</p>
+    <p>Requested content not found.</p>
+</div>
+`
+    }
+];
+
+// Create new router instance
 let router = buildBlueRouter( options );
 
