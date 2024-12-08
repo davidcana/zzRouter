8ccaef7a7defb024eff681cdc81b706da0f14e8f
David Cana <david.javapagetemplates@gmail.com>
2024-12-08 20:56:17 +0100

    Div definition with page class and id removed in page content, it is built in router.

--- samples/sample.js
+++ samples/sample.js
@@ -14,21 +14,19 @@ options.routes = [
     {
         'path': '',
         'content': `
-<div class="page" id="home">
-    <h1>Blue router sample</h1>
+<h1>Blue router sample</h1>
 
-    <nav>
-        <a href="!">Home</a> |
-        <a href="!links">Links</a> |
-        <a href="!textWriter?a=1&b=2">Text writer</a>
-    </nav>
+<nav>
+    <a href="!">Home</a> |
+    <a href="!links">Links</a> |
+    <a href="!textWriter?a=1&b=2">Text writer</a>
+</nav>
 
-    <div class="page-content">
-        <h3>Home page</h3>
-        <p>
-            This is the Home page
-        </p>
-    </div>
+<div class="page-content">
+    <h3>Home page</h3>
+    <p>
+        This is the Home page
+    </p>
 </div>
 `
     },
@@ -36,40 +34,38 @@ options.routes = [
     {
         'path': 'links',
         'content': `
-<div class="page" id="links">
-    <h1>Blue router sample</h1>
+<h1>Blue router sample</h1>
 
-    <nav>
-        <a href="!">Home</a> |
-        <a href="!links">Links</a> |
-        <a href="!textWriter">Text writer</a>
-    </nav>
+<nav>
+    <a href="!">Home</a> |
+    <a href="!links">Links</a> |
+    <a href="!textWriter">Text writer</a>
+</nav>
 
-    <div class="page-content">
-        <h3>Links page</h3>
-        <p>
-            This is the links page
-        </p>
+<div class="page-content">
+    <h3>Links page</h3>
+    <p>
+        This is the links page
+    </p>
 
-        <ul>
-            <li>
-                <a href="https://github.com/davidcana/blueRouter">Blue router</a>.
-                A simple router component to make it easy build Single Page Applications.
-            </li>
-            <li>
-                <a href="https://davidcana.github.io/ZPT-JS/">ZPT-JS</a>.
-                Zenon Page Templates - JS (ZPT-JS) is a Javascript implementation of Zope Page Templates (ZPT).
-            </li>
-            <li>
-                <a href="https://github.com/davidcana/ZPT-Java">ZPT-Java</a>.
-                JPT is a Java implementation of Zope Page Templates (ZPT).
-            </li>
-            <li>
-                <a href="https://github.com/davidcana/zzDOM">zzDOM</a>.
-                A tiny javascript API that implements only the DOM functions of jquery including chaining.
-            </li>
-        </ul>
-    </div>
+    <ul>
+        <li>
+            <a href="https://github.com/davidcana/blueRouter">Blue router</a>.
+            A simple router component to make it easy build Single Page Applications.
+        </li>
+        <li>
+            <a href="https://davidcana.github.io/ZPT-JS/">ZPT-JS</a>.
+            Zenon Page Templates - JS (ZPT-JS) is a Javascript implementation of Zope Page Templates (ZPT).
+        </li>
+        <li>
+            <a href="https://github.com/davidcana/ZPT-Java">ZPT-Java</a>.
+            JPT is a Java implementation of Zope Page Templates (ZPT).
+        </li>
+        <li>
+            <a href="https://github.com/davidcana/zzDOM">zzDOM</a>.
+            A tiny javascript API that implements only the DOM functions of jquery including chaining.
+        </li>
+    </ul>
 </div>
 `
     },
@@ -77,33 +73,31 @@ options.routes = [
     {
         'path': 'textWriter',
         'content': `
-<div class="page" id="textWriter">
-    <h1>Blue router sample</h1>
+<h1>Blue router sample</h1>
 
-    <nav>
-        <a href="!">Home</a> |
-        <a href="!links">Links</a> |
-        <a href="!textWriter">Text writer</a>
-    </nav>
+<nav>
+    <a href="!">Home</a> |
+    <a href="!links">Links</a> |
+    <a href="!textWriter">Text writer</a>
+</nav>
 
-    <div class="page-content">
-        <h3>Text writer page</h3>
-        <p>
-            This is the text writer page. Write text and click 'Add text' button or press 'Enter' to add text.
-        </p>
+<div class="page-content">
+    <h3>Text writer page</h3>
+    <p>
+        This is the text writer page. Write text and click 'Add text' button or press 'Enter' to add text.
+    </p>
 
-        <div class="field">
-            <div>Text</div>
-            <div>
-                <input type="text" id="textWriter_textToAdd" name="textWriter_textToAdd" required>
-                <button id="textWriter_addTextButton">Add text</button>
-            </div>
+    <div class="field">
+        <div>Text</div>
+        <div>
+            <input type="text" id="textWriter_textToAdd" name="textWriter_textToAdd" required>
+            <button id="textWriter_addTextButton">Add text</button>
         </div>
+    </div>
 
-        <div class="field">
-            <div>History</div>
-            <div id="textWriter_history"></div>
-        </div>
+    <div class="field">
+        <div>History</div>
+        <div id="textWriter_history"></div>
     </div>
 </div>
 `
@@ -112,24 +106,22 @@ options.routes = [
     {
         'path': '(404)',
         'content': `
-<div class="page" id="404page">
-    <h1>Blue router sample</h1>
+<h1>Blue router sample</h1>
 
-    <nav>
-        <a href="!">Home</a> |
-        <a href="!links">Links</a> |
-        <a href="!textWriter">Text writer</a>
-    </nav>
+<nav>
+    <a href="!">Home</a> |
+    <a href="!links">Links</a> |
+    <a href="!textWriter">Text writer</a>
+</nav>
 
-    <div class="page-content">
-        <h3>404 page</h3>
-        <p>
-            Sorry
-        </p>
-        <p>
-            Requested content not found.
-        </p>
-    </div>
+<div class="page-content">
+    <h3>404 page</h3>
+    <p>
+        Sorry
+    </p>
+    <p>
+        Requested content not found.
+    </p>
 </div>
 `
     }
