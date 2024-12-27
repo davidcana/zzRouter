55edd4767e8f40ae4f512467e429d4560fe48bea
David Cana <david.javapagetemplates@gmail.com>
2024-12-27 14:08:33 +0100

    New test file added: all. Some typos fixed.

---
+++ test/js/all.js
@@ -0,0 +1,16 @@
+QUnit.testSuites([
+    { name: "No transition navigation Test Suite", path: "./noTransitionNavigation.html" },
+    { name: "Both transition navigation Test Suite", path: "./bothTransitionNavigation.html" },
+    { name: "In transition navigation Test Suite", path: "./inTransitionNavigation.html" },
+    { name: "Out transition navigation Test Suite", path: "./outTransitionNavigation.html" },
+
+    { name: "No transition events Test Suite", path: "./noTransitionEvents.html" },
+    { name: "Both transition events Test Suite", path: "./bothTransitionEvents.html" },
+    { name: "In transition events Test Suite", path: "./inTransitionEvents.html" },
+    { name: "Out transition events Test Suite", path: "./outTransitionEvents.html" },
+
+    { name: "No transition render Test Suite", path: "./noTransitionRender.html" },
+    { name: "Both transition render Test Suite", path: "./bothTransitionRender.html" },
+    { name: "In transition render Test Suite", path: "./inTransitionRender.html" },
+    { name: "Out transition render Test Suite", path: "./outTransitionRender.html" }
+]);
